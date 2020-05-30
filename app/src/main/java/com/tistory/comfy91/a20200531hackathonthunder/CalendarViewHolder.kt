c89e33641323378025ddd5cb336a558d4629fbf8
KimSuhyeon <kshvv975@gmail.com>
2020-05-31 02:33:48 +0900

    CalendarViewHolder bind 함수 헤더만 만듬 - 구현 필요

---
+++ app/src/main/java/com/tistory/comfy91/a20200531hackathonthunder/CalendarViewHolder.kt
@@ -0,0 +1,11 @@
+package com.tistory.comfy91.a20200531hackathonthunder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+ class CalendarViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+     fun bind(){
+
+     }
+
+}
