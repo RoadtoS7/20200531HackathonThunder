e13a15821fc948ab39aa625821f104e4a470c869
devhapeng <hayoung8602@gmail.com>
2020-05-31 07:29:31 +0900

    7th commit

--- app/src/main/java/com/tistory/comfy91/a20200531hackathonthunder/WriteActivity.kt
+++ app/src/main/java/com/tistory/comfy91/a20200531hackathonthunder/WriteActivity.kt
@@ -54,58 +54,58 @@ class WriteActivity : AppCompatActivity() {
         }
 
             image1.setOnClickListener {
-                showToast("중식이 클릭되었습니다.")
+                showToast("한식이 클릭되었습니다.")
 
                 //인증 상품은 눌린채로, 나머지는 non으로
-                image1.setImageResource(R.drawable.click)
-                image2.setImageResource(R.drawable.nonclick)
-                image3.setImageResource(R.drawable.nonclick)
-                image4.setImageResource(R.drawable.nonclick)
-                image5.setImageResource(R.drawable.nonclick)
+                image1.setImageResource(R.drawable.ic_korean)
+                image2.setImageResource(R.drawable.ic_chineseblack)
+                image3.setImageResource(R.drawable.ic_japaneseblack)
+                image4.setImageResource(R.drawable.ic_italian_black)
+                image5.setImageResource(R.drawable.ic_ddeck_black)
             }
 
             image2.setOnClickListener {
-                showToast("양식이 클릭되었습니다.")
+                showToast("중식이 클릭되었습니다.")
 
                 //인증 상품은 눌린채로, 나머지는 non으로
-                image1.setImageResource(R.drawable.nonclick)
-                image2.setImageResource(R.drawable.click)
-                image3.setImageResource(R.drawable.nonclick)
-                image4.setImageResource(R.drawable.nonclick)
-                image5.setImageResource(R.drawable.nonclick)
+                image1.setImageResource(R.drawable.ic_koreanblack)
+                image2.setImageResource(R.drawable.ic_chinese)
+                image3.setImageResource(R.drawable.ic_japaneseblack)
+                image4.setImageResource(R.drawable.ic_italian_black)
+                image5.setImageResource(R.drawable.ic_ddeck_black)
             }
 
             image3.setOnClickListener {
                 showToast("일식이 클릭되었습니다.")
 
                 //인증 상품은 눌린채로, 나머지는 non으로
-                image1.setImageResource(R.drawable.nonclick)
-                image2.setImageResource(R.drawable.nonclick)
-                image3.setImageResource(R.drawable.click)
-                image4.setImageResource(R.drawable.nonclick)
-                image5.setImageResource(R.drawable.nonclick)
+                image1.setImageResource(R.drawable.ic_koreanblack)
+                image2.setImageResource(R.drawable.ic_chineseblack)
+                image3.setImageResource(R.drawable.ic_japanese)
+                image4.setImageResource(R.drawable.ic_italian_black)
+                image5.setImageResource(R.drawable.ic_ddeck_black)
             }
 
             image4.setOnClickListener {
-                showToast("한식이 클릭되었습니다.")
+                showToast("양식이 클릭되었습니다.")
 
                 //인증 상품은 눌린채로, 나머지는 non으로
-                image1.setImageResource(R.drawable.nonclick)
-                image2.setImageResource(R.drawable.nonclick)
-                image3.setImageResource(R.drawable.nonclick)
-                image4.setImageResource(R.drawable.click)
-                image5.setImageResource(R.drawable.nonclick)
+                image1.setImageResource(R.drawable.ic_koreanblack)
+                image2.setImageResource(R.drawable.ic_chineseblack)
+                image3.setImageResource(R.drawable.ic_japaneseblack)
+                image4.setImageResource(R.drawable.ic_italian)
+                image5.setImageResource(R.drawable.ic_ddeck_black)
             }
 
             image5.setOnClickListener {
-                showToast("기타가 클릭되었습니다.")
+                showToast("분식이 클릭되었습니다.")
 
                 //인증 상품은 눌린채로, 나머지는 non으로
-                image1.setImageResource(R.drawable.nonclick)
-                image2.setImageResource(R.drawable.nonclick)
-                image3.setImageResource(R.drawable.nonclick)
-                image4.setImageResource(R.drawable.nonclick)
-                image5.setImageResource(R.drawable.click)
+                image1.setImageResource(R.drawable.ic_koreanblack)
+                image2.setImageResource(R.drawable.ic_chineseblack)
+                image3.setImageResource(R.drawable.ic_japaneseblack)
+                image4.setImageResource(R.drawable.ic_italian_black)
+                image5.setImageResource(R.drawable.ic_bunsik)
             }
         }
     }
